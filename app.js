5d841ccecbcee5b713ea08a9e4bbe3d93a8ba7dc
miomayska18 <miomayska18@codingburgas.bg>
2022-11-24 10:21:51 +0200

    Add more stuff

---
+++ app.js
@@ -0,0 +1,8 @@
+const accordian = document.querySelector('.content-container');
+
+for(i = 0; i < accordian.length; i++)
+{
+    accordian[i].addEventListener('click', function(){
+        this.classList.toggle('activate');
+    });
+}
